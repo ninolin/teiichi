393072726e09ff07958b9a32b85adc826259edc5
ninolin <a7872278722@gmail.com>
2018-07-25 22:10:25 +0800

    sign 14

--- receiveline.php
+++ receiveline.php
@@ -235,6 +235,8 @@
 		if($result->num_rows == 0){
 			$sql = "INSERT INTO line_user_sign (line_id, sign_date) 
 				VALUES ('".$sender_userid."', '".$date("Y-m-d")."')";
+			$myfile = fopen("log2.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+			fwrite($myfile, "\xEF\xBB\xBF".$sql); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
 			$result = sql_select_fetchALL($sql);
 			$json_str = '{
 				"type": "text",
