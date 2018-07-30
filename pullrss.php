f1efbffae90c2b3e15d8ec49748d6f1a71f9f57f
Nino Lin <nino.lin@isuncloud.com>
2018-07-30 17:23:31 +0800

    pullrss 3

--- pullrss.php
+++ pullrss.php
@@ -2,98 +2,61 @@
 <?php
 
 //RSS源地址列表數組
-
 $rssfeed = array("https://www.google.com.tw/alerts/feeds/10688482321941126506/10267095873014584322");
-
 
 
 //設置編碼為UTF-8
 header('Content-Type:text/html;charset= UTF-8');
-
 for($i=0;$i<sizeof($rssfeed);$i++){//分解開始
 
-$buff = "";
+    $buff = "";
+    $rss_str="";
 
-$rss_str="";
+    //打開rss地址，並讀取，讀取失敗則中止
+    $fp = fopen($rssfeed[$i],"r") or die("can not open $rssfeed");
+    while ( !feof($fp) ) {
+        $buff .= fgets($fp,4096);
+    }
 
-//打開rss地址，並讀取，讀取失敗則中止
+    //關閉文件打開
+    fclose($fp);
 
-$fp = fopen($rssfeed[$i],"r") or die("can not open $rssfeed");
+    //建立一個 XML 解析器
+    $parser = xml_parser_create();
 
-while ( !feof($fp) ) {
+    //xml_parser_set_option -- 為指定 XML 解析進行選項設置
+    xml_parser_set_option($parser,XML_OPTION_SKIP_WHITE,1);
+    //xml_parse_into_struct -- 將 XML 數據解析到數組$values中
+    xml_parse_into_struct($parser,$buff,$values,$idx);
+    //xml_parser_free -- 釋放指定的 XML 解析器
+    xml_parser_free($parser);
 
-$buff .= fgets($fp,4096);
+    print_r($values);
+    foreach ($values as $val) {
+        $tag = $val["tag"];
+        $type = $val["type"];
+        $value = $val["value"];
 
-}
+        //標籤統一轉為小寫
+        $tag = strtolower($tag);
+        if ($tag == "item" && $type == "open"){
+            $is_item = 1;
+        }else if ($tag == "item" && $type == "close") {
+            //構造輸出字符串
+            $rss_str .= "<a href='".$link."' target=_blank>".$title."</a>";
+            $is_item = 0;
+        }
 
-//關閉文件打開
+        //僅讀取item標籤中的內容
 
-fclose($fp);
+        if($is_item==1){
+            if ($tag == "title") {$title = $value;}
+            if ($tag == "link") {$link = $value;}
+        }
+    }
 
-
-
-//建立一個 XML 解析器
-
-$parser = xml_parser_create();
-
-//xml_parser_set_option -- 為指定 XML 解析進行選項設置
-
-xml_parser_set_option($parser,XML_OPTION_SKIP_WHITE,1);
-
-//xml_parse_into_struct -- 將 XML 數據解析到數組$values中
-
-xml_parse_into_struct($parser,$buff,$values,$idx);
-
-//xml_parser_free -- 釋放指定的 XML 解析器
-
-xml_parser_free($parser);
-
-
-
-foreach ($values as $val) {
-
-$tag = $val["tag"];
-
-$type = $val["type"];
-
-$value = $val["value"];
-
-//標籤統一轉為小寫
-
-$tag = strtolower($tag);
-
-
-
-if ($tag == "item" && $type == "open"){
-
-$is_item = 1;
-
-}else if ($tag == "item" && $type == "close") {
-
-//構造輸出字符串
-
-$rss_str .= "<a href='".$link."' target=_blank>".$title."</a>
-";
-
-$is_item = 0;
-
-}
-
-//僅讀取item標籤中的內容
-
-if($is_item==1){
-
-if ($tag == "title") {$title = $value;}
-
-if ($tag == "link") {$link = $value;}
-
-}
-
-}
-
-//輸出結果
-
-echo $rss_str."";
+    //輸出結果
+    echo $rss_str."";
 
 }
 ?>
