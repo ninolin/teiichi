69335c5c36b008578d382ffc58c572a8fb119676
Nino Lin <nino.lin@isuncloud.com>
2018-08-04 14:20:41 +0800

    commit test

--- receiveline.php
+++ receiveline.php
@@ -177,6 +177,8 @@
 	//每日簽到
 	function sign($sender_userid){
 		$sql = "SELECT * FROM line_user WHERE line_id ='".$sender_userid."'";
+		$myfile = fopen("log2.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+		fwrite($myfile, "\xEF\xBB\xBF".$sql); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
 		$result = sql_select_fetchALL($sql);
 		if($result->num_rows == 0){
 			$json_str = '{
