6f21372a7079c6e30a94e19e09df7999640ecf6c
Nino Lin <nino.lin@isuncloud.com>
2018-08-04 17:34:45 +0800

    commit test

--- receiveline.php
+++ receiveline.php
@@ -317,7 +317,7 @@
 			foreach($result as $a){
 				$user_created_date = $a['user_created_date'];
 			}
-			$myfile = fopen("log2.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+
 			fwrite($myfile, "\xEF\xBB\xBF abc".$user_created_date); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
 			//expire_date
 			$expire_date = 0;
