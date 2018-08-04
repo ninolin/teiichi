0e69bb48c9d882568722efbad2973360d2cedf9e
Nino Lin <nino.lin@isuncloud.com>
2018-08-04 14:39:10 +0800

    commit test

--- signup.php
+++ signup.php
@@ -4,14 +4,15 @@
     $name = $_POST['name'];
     $phone = $_POST['phone'];
     
- $myfile = fopen("log2.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
-		fwrite($myfile, "\xEF\xBB\xBF".$line_id); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
+ 
 
     if($type == 'add'){
         $sql = "INSERT INTO line_user 
                 (line_id, name, phone, user_created_date) 
             VALUES 
                 ('".$line_id."', '".$name."', '".$phone."', ".date("Y-m-d").")";
+        $myfile = fopen("log2.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
+		fwrite($myfile, "\xEF\xBB\xBF".$sql); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
         sql_select_fetchALL($sql);
 	    $myfile = fopen("log2.txt", "w+") or die("Unable to open file!"); //設定一個log.txt來印訊息
 		fwrite($myfile, "\xEF\xBB\xBF".$sql); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
